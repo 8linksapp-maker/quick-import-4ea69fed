0f3c28b4104691cd9cf4192b8f28ba35a8de123e
Seu Nome ou Nick do GitHub <novaeracomvisual@gmail.com>
2025-11-19 14:12:26 -0300

    fix: Corrige caminho de importação do supabaseClient em AddWpSiteForm

--- components/AddWpSiteForm.tsx
+++ components/AddWpSiteForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../../src/supabaseClient';
+import { supabase } from '../src/supabaseClient';
 import InputField from '../InputField';
 
 interface AddWpSiteFormProps {
